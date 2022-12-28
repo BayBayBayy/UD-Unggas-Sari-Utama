f03e877a9f380f66e07f4ab7e9b9b2527891c571
Bayu Adnyana <102949541+BayBayBayy@users.noreply.github.com>
2022-12-28 12:50:13 +0800

    Project Start
    Astungkara selesai lebih cepat

---
+++ UD Unggas Sari Utama/UD Unggas Sari Utama/UD_Unggas_Sari_UtamaApp.swift
@@ -0,0 +1,20 @@
+//
+//  UD_Unggas_Sari_UtamaApp.swift
+//  UD Unggas Sari Utama
+//
+//  Created by I Wayan Adnyana on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UD_Unggas_Sari_UtamaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
